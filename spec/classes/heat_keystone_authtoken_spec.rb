9700e19e1493c7f78eabe0925b510967d34b9572
Mykyta Karpin <mkarpin@mirantis.com>
2016-11-11 13:11:39 +0200

    Switch heat to rspec-puppet-facts
    This change updates the heat tests to use rspec-puppet-facts
    to test multiple operating systems.
    
    Change-Id: Ic6e1542d5befba529f753b346ff842ed88d1292e

--- spec/classes/heat_keystone_authtoken_spec.rb
+++ spec/classes/heat_keystone_authtoken_spec.rb
@@ -143,7 +143,7 @@ describe 'heat::keystone::authtoken' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'heat authtoken'
+      it_behaves_like 'heat authtoken'
     end
   end
 
